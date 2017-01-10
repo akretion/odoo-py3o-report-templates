039e5aac3a1b0fbda9a3944fd9228487507efd1d
Alexis de Lattre <alexis.delattre@akretion.com>
2017-01-10 22:44:01 +0100

    Add module account_check_deposit_py3o
    Improve all reports (relative size for tables, ...)

---
+++ account_check_deposit_py3o/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Check Deposit Py3o',
+    'version': '10.0.1.0.0',
+    'category': 'Accounting',
+    'license': 'AGPL-3',
+    'summary': 'Sample py3o check deposit report',
+    'description': """
+Check Deposit Py3o
+==================
+
+This module adds a sample py3o check deposit report in French.
+
+This module has been written by Alexis de Lattre from Akretion
+<alexis.delattre@akretion.com>.
+    """,
+    'author': 'Akretion',
+    'depends': [
+        'report_py3o',
+        'base_usability',  # for header
+        'account_check_deposit',
+        ],
+    'data': ['report.xml'],
+    'installable': True,
+}
