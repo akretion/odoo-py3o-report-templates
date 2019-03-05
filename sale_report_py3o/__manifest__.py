e9469e479b62d10b88f1e4bd49a6aa42678d20af
Alexis de Lattre <alexis.delattre@akretion.com>
2019-03-05 17:19:02 +0100

    MIG sale_report_py3o

--- sale_report_py3o/__manifest__.py
+++ sale_report_py3o/__manifest__.py
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*-
-# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2016-2019 Akretion France (https://akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Sale Reports Py3o',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Sales',
     'license': 'AGPL-3',
     'summary': 'Sample py3o sale reports',
@@ -27,5 +27,5 @@ This module has been written by Alexis de Lattre from Akretion
         'account_payment_sale',
         ],
     'data': ['report.xml'],
-    'installable': False,
+    'installable': True,
 }
