4e61e25afa3e250a7e267d8e4c14297abe362410
Alexis de Lattre <alexis.delattre@akretion.com>
2021-02-17 15:15:40 +0100

    [MIG] account_invoice_report_py3o

--- account_invoice_report_py3o/__manifest__.py
+++ account_invoice_report_py3o/__manifest__.py
@@ -1,9 +1,10 @@
-# Copyright 2016-2019 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2016-2021 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Invoice Report Py3o',
-    'version': '12.0.1.0.0',
+    'version': '14.0.1.0.0',
     'category': 'Accounting',
     'license': 'AGPL-3',
     'summary': 'Sample py3o invoice report',
@@ -25,5 +26,5 @@ This module has been written by Alexis de Lattre from Akretion
         'account_payment_partner',
         ],
     'data': ['report.xml'],
-    'installable': False,
+    'installable': True,
 }
