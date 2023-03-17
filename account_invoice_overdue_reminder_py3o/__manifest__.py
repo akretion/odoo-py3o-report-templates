e599394a13bd121ed11c6c156c9108ad1edf7645
Alexis de Lattre <alexis.delattre@akretion.com>
2023-03-17 12:32:50 +0100

    Add user_id in purchase.order reports
    Other small improvements

--- account_invoice_overdue_reminder_py3o/__manifest__.py
+++ account_invoice_overdue_reminder_py3o/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2020-2021 Akretion France (http://www.akretion.com/)
+# Copyright 2020-2023 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Invoice Overdue Reminder Py3o',
-    'version': '14.0.1.0.0',
+    'version': '16.0.1.0.0',
     'category': 'Accounting',
     'license': 'AGPL-3',
     'summary': 'Sample py3o overdue reminder letter report',
